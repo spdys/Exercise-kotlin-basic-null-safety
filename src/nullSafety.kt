8f0449497398b9d77e2d6b83204ec0f4aa47955c
spdys <noorii549@yahoo.com>
2025-02-18 17:48:45 +0300

    Finished final step 5 tested with both valid and null score

--- src/nullSafety.kt
+++ src/nullSafety.kt
@@ -1,5 +1,10 @@
+fun nullDouble(score: Double?): Double {
+    return score ?: 10.48
+}
+
 fun main() {
     var city: String? = "Kuwait City"
     city = null
     println(city?.length ?: 55)
+    println(nullDouble(19.999))
 }
